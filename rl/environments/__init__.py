a2093d205a36294883aaf248933c51168253d32c
VincentFokker <44567443+VincentFokker@users.noreply.github.com>
2020-09-10 10:23:10 +0200

    up

--- rl/environments/__init__.py
+++ rl/environments/__init__.py
@@ -1,3 +1,3 @@
 # from .TestEnv import TestEnv
-#from .simple_conveyor_2 import simple_conveyor_2
-from .simple_conveyor_1 import simple_conveyor_1
+from .simple_conveyor_2 import simple_conveyor_2
+#from .simple_conveyor_1 import simple_conveyor_1
