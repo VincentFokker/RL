ef3b7491eacfdc6f9b38c7cc01bd9b96748a7089
VincentFokker <44567443+VincentFokker@users.noreply.github.com>
2020-09-25 07:53:30 +0200

    up

--- scheduler.py
+++ scheduler.py
@@ -21,6 +21,7 @@ Usage:
 def run_experiment(env_name, parameter_folder , result_folder, verbose=False):
     models = []
     log = []
+    start = time.time()
     parameter_dir = os.path.join(settings.CONFIG, parameter_folder)
     for param_file in os.listdir(parameter_dir):
         parameters = os.path.join(parameter_dir, param_file)
@@ -37,11 +38,13 @@ def run_experiment(env_name, parameter_folder , result_folder, verbose=False):
         print('Training time: {:2f} min, steps/s: {}'.format(t/60, float(steps)/t), sep)
         log.append('Training time for model {}: {:2f} min, steps/s: {}'.format(i, t/60, float(steps)/t))
 
+    end = (time.time() - start)/60
     path = os.path.join(settings.TRAINED_MODELS, env_name)
     path = os.path.join(path, result_folder)
     with open(os.path.join(path, 'training_log.txt'), 'w') as f:
         for item in log:
             f.write("%s\n" % item)
+        f.write("Total runtime: {} minutes".format(end))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
