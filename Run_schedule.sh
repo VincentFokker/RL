5a553d98c1ad244d0513200b67c7a4888f289baf
VincentFokker <44567443+VincentFokker@users.noreply.github.com>
2020-11-23 14:25:14 +0100

    up

---
+++ Run_schedule.sh
@@ -0,0 +1,20 @@
+python train2.py -e AbstractConveyor -s 20201123_1417 -n 0|-2|0|-7|0|-10 	 -c config1
+python plotmaker.py -e AbstractConveyor -s 20201123_1417
+python train2.py -e AbstractConveyor -s 20201123_1507 -n 10|-1|-2|-7|-7|-10 	 -c config2
+python plotmaker.py -e AbstractConveyor -s 20201123_1507
+python train2.py -e AbstractConveyor -s 20201123_1557 -n 10|-7|-1|-1|-1|-10 	 -c config3
+python plotmaker.py -e AbstractConveyor -s 20201123_1557
+python train2.py -e AbstractConveyor -s 20201123_1647 -n 2|-5|-5|-5|-1|-5 	 -c config4
+python plotmaker.py -e AbstractConveyor -s 20201123_1647
+python train2.py -e AbstractConveyor -s 20201123_1737 -n 10|-5|-2|-1|-2|-1 	 -c config5
+python plotmaker.py -e AbstractConveyor -s 20201123_1737
+python train2.py -e AbstractConveyor -s 20201123_1827 -n 7|-2|-7|-1|-1|-1 	 -c config6
+python plotmaker.py -e AbstractConveyor -s 20201123_1827
+python train2.py -e AbstractConveyor -s 20201123_1917 -n 10|-10|-10|-2|-10|-5 	 -c config7
+python plotmaker.py -e AbstractConveyor -s 20201123_1917
+python train2.py -e AbstractConveyor -s 20201123_2007 -n 7|-1|-10|-5|-5|-10 	 -c config8
+python plotmaker.py -e AbstractConveyor -s 20201123_2007
+python train2.py -e AbstractConveyor -s 20201123_2057 -n 2|-7|-10|-1|-1|-1 	 -c config9
+python plotmaker.py -e AbstractConveyor -s 20201123_2057
+python train2.py -e AbstractConveyor -s 20201123_2147 -n 10|-1|-1|-7|-5|0 	 -c config10
+python plotmaker.py -e AbstractConveyor -s 20201123_2147
