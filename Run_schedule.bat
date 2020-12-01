c7dcc078199fbaf47142f5b878731844a65514aa
VincentFokker <44567443+VincentFokker@users.noreply.github.com>
2020-12-01 09:13:18 +0100

    up

--- Run_schedule.bat
+++ Run_schedule.bat
@@ -1,31 +1,8 @@
-python train2.py -e AbstractConveyor -s 20201124_2200 -n 3_4_5_8_9_11_12 	 -c config1
-python plotmaker.py -e AbstractConveyor -s 20201124_2200
-python train2.py -e AbstractConveyor -s 20201124_2250 -n 4_8_11_12 	 -c config2
-python plotmaker.py -e AbstractConveyor -s 20201124_2250
-python train2.py -e AbstractConveyor -s 20201124_2340 -n 2_6_8_9_10_11_12 	 -c config3
-python plotmaker.py -e AbstractConveyor -s 20201124_2340
-python train2.py -e AbstractConveyor -s 20201125_0030 -n 1_2_3_6_7_8_9_10 	 -c config4
-python plotmaker.py -e AbstractConveyor -s 20201125_0030
-python train2.py -e AbstractConveyor -s 20201125_0120 -n 1_2_3_5_9_11 	 -c config5
-python plotmaker.py -e AbstractConveyor -s 20201125_0120
-python train2.py -e AbstractConveyor -s 20201125_0210 -n 2_7_8 	 -c config6
-python plotmaker.py -e AbstractConveyor -s 20201125_0210
-python train2.py -e AbstractConveyor -s 20201125_0300 -n 1_5_6_7_8_10 	 -c config7
-python plotmaker.py -e AbstractConveyor -s 20201125_0300
-python train2.py -e AbstractConveyor -s 20201125_0350 -n 4_7_8_9_11 	 -c config8
-python plotmaker.py -e AbstractConveyor -s 20201125_0350
-python train2.py -e AbstractConveyor -s 20201125_0440 -n 1_3_6_10_11_12 	 -c config9
-python plotmaker.py -e AbstractConveyor -s 20201125_0440
-python train2.py -e AbstractConveyor -s 20201125_0530 -n 1_3_4_7_8_10_11 	 -c config10
-python plotmaker.py -e AbstractConveyor -s 20201125_0530
-python train2.py -e AbstractConveyor -s 20201125_0620 -n 1_5_6_12 	 -c config11
-python plotmaker.py -e AbstractConveyor -s 20201125_0620
-python train2.py -e AbstractConveyor -s 20201125_0710 -n 3_4_6_8_10_11_12 	 -c config12
-python plotmaker.py -e AbstractConveyor -s 20201125_0710
-python train2.py -e AbstractConveyor -s 20201125_0800 -n 2_3_4_6_8_11 	 -c config13
-python plotmaker.py -e AbstractConveyor -s 20201125_0800
-python train2.py -e AbstractConveyor -s 20201125_0850 -n 2_3_4_6_7_8_10 	 -c config14
-python plotmaker.py -e AbstractConveyor -s 20201125_0850
-python train2.py -e AbstractConveyor -s 20201125_0940 -n 5_6_7_9_12 	 -c config15
-python plotmaker.py -e AbstractConveyor -s 20201125_0940
+python train2.py -e AbstractConveyor1 -s 20201128_1200 -n 7_0_5_13_11_8 	 -c config1
+python plotmaker.py -e AbstractConveyor1 -s 20201128_1200
+python train2.py -e AbstractConveyor1 -s 20201128_1250 -n 15_6_6_15_9_6 	 -c config2
+python plotmaker.py -e AbstractConveyor1 -s 20201128_1250
+python train2.py -e AbstractConveyor1 -s 20201128_1340 -n 7_0_5_13_11_8 	 -c config3
+python plotmaker.py -e AbstractConveyor1 -s 20201128_1340
+
 PAUSE
