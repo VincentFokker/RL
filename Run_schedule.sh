4d33cdfcfb93d1f3c59a90032734ba34d39bab1b
VincentFokker <44567443+VincentFokker@users.noreply.github.com>
2020-12-17 14:30:18 +0100

    up

--- Run_schedule.sh
+++ Run_schedule.sh
@@ -1,20 +1,12 @@
-python train2.py -e AbstractConveyor -s 20201123_1417 -n '0|-2|0|-7|0|-10' 	 -c config1
-python plotmaker.py -e AbstractConveyor -s 20201123_1417
-python train2.py -e AbstractConveyor -s 20201123_1507 -n '10|-1|-2|-7|-7|-10' 	 -c config2
-python plotmaker.py -e AbstractConveyor -s 20201123_1507
-python train2.py -e AbstractConveyor -s 20201123_1557 -n '10|-7|-1|-1|-1|-10'	 -c config3
-python plotmaker.py -e AbstractConveyor -s 20201123_1557
-python train2.py -e AbstractConveyor -s 20201123_1647 -n '2|-5|-5|-5|-1|-5' 	 -c config4
-python plotmaker.py -e AbstractConveyor -s 20201123_1647
-python train2.py -e AbstractConveyor -s 20201123_1737 -n '10|-5|-2|-1|-2|-1' 	 -c config5
-python plotmaker.py -e AbstractConveyor -s 20201123_1737
-python train2.py -e AbstractConveyor -s 20201123_1827 -n '7|-2|-7|-1|-1|-1 '	 -c config6
-python plotmaker.py -e AbstractConveyor -s 20201123_1827
-python train2.py -e AbstractConveyor -s 20201123_1917 -n '10|-10|-10|-2|-10|-5' 	 -c config7
-python plotmaker.py -e AbstractConveyor -s 20201123_1917
-python train2.py -e AbstractConveyor -s 20201123_2007 -n '7|-1|-10|-5|-5|-10' 	 -c config8
-python plotmaker.py -e AbstractConveyor -s 20201123_2007
-python train2.py -e AbstractConveyor -s 20201123_2057 -n '2|-7|-10|-1|-1|-1' 	 -c config9
-python plotmaker.py -e AbstractConveyor -s 20201123_2057
-python train2.py -e AbstractConveyor -s 20201123_2147 -n '10|-1|-1|-7|-5|0' 	 -c config10
-python plotmaker.py -e AbstractConveyor -s 20201123_2147
+python train.py -e AbstractConveyor1 -s 20201217_1400 -n buffer1 	 -c buffer1
+python train.py -e AbstractConveyor1 -s 20201217_1500 -n buffer2 	 -c buffer2
+python train.py -e AbstractConveyor1 -s 20201217_1600 -n buffer3 	 -c buffer3
+python train.py -e AbstractConveyor1 -s 20201217_1700 -n buffer4 	 -c buffer4
+python train.py -e AbstractConveyor1 -s 20201217_1800 -n buffer5 	 -c buffer5
+python train.py -e AbstractConveyor1 -s 20201217_1900 -n buffer6 	 -c buffer6
+python train.py -e AbstractConveyor1 -s 20201217_2000 -n buffer7 	 -c buffer7
+python train.py -e AbstractConveyor1 -s 20201217_2100 -n buffer8 	 -c buffer8
+python train.py -e AbstractConveyor1 -s 20201217_2200 -n buffer9 	 -c buffer9
+python train.py -e AbstractConveyor1 -s 20201217_2300 -n buffer10 	 -c buffer10
+
+PAUSE
