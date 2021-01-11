f563041c0d88bad1b468ecac2607ad6a419ac851
VincentFokker <44567443+VincentFokker@users.noreply.github.com>
2021-01-11 22:38:50 +0100

    up

--- run_schedule.bat
+++ run_schedule.bat
@@ -1,12 +1,6 @@
-python expert_trajectories.py   -e ConveyorEnv1 -s 20210110_1208 -n 500 -c config1
-python pretrain.py              -e ConveyorEnv1 -s 20210110_1208 -c config1
-python expert_trajectories.py   -e ConveyorEnv1 -s 20210110_1209 -n 500 -c config2
-python pretrain.py              -e ConveyorEnv1 -s 20210110_1209 -c config2
-python expert_trajectories.py   -e ConveyorEnv1 -s 20210110_1210 -n 500 -c config3
-python pretrain.py              -e ConveyorEnv1 -s 20210110_1210 -c config3
-python expert_trajectories.py   -e ConveyorEnv1 -s 20210110_1211 -n 500 -c config4
-python pretrain.py              -e ConveyorEnv1 -s 20210110_1211 -c config4
-python expert_trajectories.py   -e ConveyorEnv1 -s 20210110_1212 -n 500 -c config5
-python pretrain.py              -e ConveyorEnv1 -s 20210110_1212 -c config5
-python expert_trajectories.py   -e ConveyorEnv1 -s 20210110_1213 -n 500 -c config6
-python pretrain.py              -e ConveyorEnv1 -s 20210110_1213 -c config6
+python train_callback.py -e ConveyorEnv12 -s 20210112_0030 -n 2x2design -t -c config1
+python train_callback.py -e ConveyorEnv12 -s 20210112_0130 -n 2x2design -t -c config2
+python train_callback.py -e ConveyorEnv12 -s 20210112_0230 -n 2x2design -t -c config3
+python train_callback.py -e ConveyorEnv12 -s 20210112_0330 -n 2x2design -t -c config4
+python train_callback.py -e ConveyorEnv12 -s 20210112_0430 -n 2x2design -t -c config5
+python train_callback.py -e ConveyorEnv12 -s 20210112_0530 -n 2x2design -t -c config6
