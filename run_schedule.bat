14343200cb6433e1070ba2f781710f1a3040a373
VincentFokker <44567443+VincentFokker@users.noreply.github.com>
2021-01-12 08:56:36 +0100

    up

--- run_schedule.bat
+++ run_schedule.bat
@@ -1,4 +1,4 @@
-python train_callback.py -e ConveyorEnv12 -s 20210112_0030 -n 2x2design -t -c config1
+    python train_callback.py -e ConveyorEnv12 -s 20210112_0030 -n 2x2design -t -c config1
 python train_callback.py -e ConveyorEnv12 -s 20210112_0130 -n 2x2design -t -c config2
 python train_callback.py -e ConveyorEnv12 -s 20210112_0230 -n 2x2design -t -c config3
 python train_callback.py -e ConveyorEnv12 -s 20210112_0330 -n 2x2design -t -c config4
